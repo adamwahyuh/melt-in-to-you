ebaf34f665e3f361ea4173145b65fd16c9a03875
Adam Hariyadi <adamwahyu875@gmail.com>
2026-07-28 19:05:38 +0700

    Cup Test

---
+++ tests/Feature/CupTest.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Cup;
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class CupTest extends TestCase
+{
+    public function test_menambahkan_ke_cup(){
+        $user = User::factory()->create([
+            'username' => 'dara',
+            'password' => Hash::make('password'),
+        ]);
+
+        $user->peran()->sync([1,2,3]);
+
+        $this->actingAs($user);
+
+        $product = Product::factory()->create();
+
+        $response = $this->post(route('post.cup.store_to_cup'), [
+            'product_id' => $product->id,
+            'quantity' => 100,
+        ]);
+
+        $response->assertRedirectBack();
+
+        $this->assertDatabaseHas('cups', [
+            'user_id' => $user->id,
+        ]);
+
+        $this->assertDatabaseHas('cup_details', [
+            'product_id' => $product->id,
+            'quantity' => 100,
+        ]);
+    }
+
+    public function test_hapus_item_dari_cup(){
+        $user = User::factory()->create([
+            'username' => 'dara',
+            'password' => Hash::make('password'),
+        ]);
+
+        $user->peran()->sync([1,2,3]);
+
+        $this->actingAs($user);
+
+        $product = Product::factory()->create();
+
+        $response = $this->post(route('post.cup.store_to_cup'), [
+            'product_id' => $product->id,
+            'quantity' => 100,
+        ]);
+
+        $cup = Cup::with('details')->where('user_id', $user->id)->first();
+
+        $cupDetail = $cup->details[0];
+
+        $response = $this->delete(route('delete.cup.delete_cup_detail', $cupDetail->id));
+
+        $response->assertRedirectBack();
+
+        $this->assertDatabaseMissing('cup_details', [
+            'product_id' => $product->id
+        ]);
+    }
+
+    public function test_memesan_dari_cup(){
+        $user = User::factory()->create([
+            'username' => 'dara',
+            'password' => Hash::make('password'),
+        ]);
+
+        $user->peran()->sync([1,2,3]);
+
+        $this->actingAs($user);
+
+        $product = Product::factory()->create();
+
+        $response = $this->post(route('post.cup.store_to_cup'), [
+            'product_id' => $product->id,
+            'quantity' => 100,
+        ]);
+
+        $response->assertRedirectBack();
+
+        $this->assertDatabaseHas('cups', [
+            'user_id' => $user->id,
+        ]);
+
+        $this->assertDatabaseHas('cup_details', [
+            'product_id' => $product->id,
+            'quantity' => 100,
+        ]);
+
+        $cup = Cup::with('details')->where('user_id', $user->id)->first();
+
+        $cupDetail = $cup->details[0];
+
+        $response = $this->post(route('post.order.transfer_cup_to_order'));
+
+        $this->assertDatabaseHas('order_details', [
+            'product_id' => $product->id,
+            'quantity' => 100,
+        ]);
+
+        $this->assertDatabaseHas('orders', [
+            'user_id' => $user->id,
+            'dipesan_pada' => now(),
+        ]);
+    }
+}
